eb8443230716da957b1afac8b1065c89e07e5f7a
ahmedd-mahmoud <ahmed.mahran831@gmail.com>
2025-02-28 14:56:09 +0200

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <h1 class="text-center text-5xl text-red-500">Vue 3 + Vite + TypeScript</h1>
+  </div>
+</template>
