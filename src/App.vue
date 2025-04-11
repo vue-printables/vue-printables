97764a449d176d8cf593c06b5c62fbaf974c28b0
AlhoussinAmin <Alhoussin@34ml.com>
2025-04-11 21:39:47 +0200

    chore:clean canvas code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import CanvasEditor from "./components/CanvasEditor.vue";
-</script>
-
 <template>
   <div class="min-h-screen bg-white p-4 sm:p-8">
     <header class="mb-8 text-center">
@@ -12,8 +7,55 @@ import CanvasEditor from "./components/CanvasEditor.vue";
       </p>
     </header>
 
-    <main>
+    <main class="flex justify-between gap-5">
+      <div class="toolbar grow rounded bg-gray-100 p-4">
+        <div class="flex p-2">
+          <button
+            class="mr-2 px-4 py-2"
+            :class="
+              activeTab === 'image'
+                ? 'border-b-2 border-blue-500 font-bold'
+                : ''
+            "
+            @click="activeTab = 'image'"
+          >
+            Image Upload
+          </button>
+          <button
+            class="px-4 py-2"
+            :class="
+              activeTab === 'text' ? 'border-b-2 border-blue-500 font-bold' : ''
+            "
+            @click="activeTab = 'text'"
+          >
+            Text Editor
+          </button>
+        </div>
+
+        <!-- Tab Content -->
+        <div v-show="activeTab === 'image'" class="tab-panel">
+          <ImageUploader :onImageUploaded="handleImageUploaded" />
+        </div>
+        <div v-show="activeTab === 'text'" class="tab-panel">
+          <TextEditor :onTextAdded="handleTextAdded" />
+        </div>
+      </div>
       <CanvasEditor />
     </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import CanvasEditor from "~/components/CanvasEditor.vue";
+import ImageUploader from "~/components/ImageUploader.vue";
+import TextEditor, { type TextConfig } from "~/components/TextEditor.vue";
+
+const activeTab = ref<"image" | "text">("image");
+
+// Handle image upload
+const handleImageUploaded = (imgElement: HTMLImageElement) => {};
+
+// Handle text addition
+const handleTextAdded = (textConfig: TextConfig) => {};
+</script>
