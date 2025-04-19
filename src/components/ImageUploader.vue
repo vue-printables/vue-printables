185cc8c3bd6a5b699500d0b0b5c5b308dfe1204d
AlhoussinAmin <Alhoussin@34ml.com>
2025-04-19 15:45:11 +0200

    chore:clean helper codes

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -1,37 +1,3 @@
-<script setup lang="ts">
-import { ref } from "vue";
-
-const props = defineProps<{
-  onImageUploaded: (image: HTMLImageElement) => void;
-}>();
-
-const fileInput = ref<HTMLInputElement | null>(null);
-
-const handleFileChange = (event: Event) => {
-  const input = event.target as HTMLInputElement;
-  if (input.files && input.files[0]) {
-    const file = input.files[0];
-    const reader = new FileReader();
-
-    reader.onload = (e) => {
-      const img = new Image();
-      img.src = e.target?.result as string;
-      img.onload = () => {
-        props.onImageUploaded(img);
-        // Reset the file input to allow uploading the same file again
-        if (fileInput.value) fileInput.value.value = "";
-      };
-    };
-
-    reader.readAsDataURL(file);
-  }
-};
-
-const triggerFileInput = () => {
-  fileInput.value?.click();
-};
-</script>
-
 <template>
   <div>
     <button
@@ -49,3 +15,37 @@ const triggerFileInput = () => {
     />
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emits = defineEmits<{
+  imageUploaded: [image: HTMLImageElement];
+}>();
+
+const fileInput = ref<HTMLInputElement | null>(null);
+
+const handleFileChange = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  if (input.files && input.files[0]) {
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const img = new Image();
+      img.src = e.target?.result as string;
+      img.onload = () => {
+        emits("imageUploaded", img);
+        // Reset the file input to allow uploading the same file again
+        if (fileInput.value) fileInput.value.value = "";
+      };
+    };
+
+    reader.readAsDataURL(file);
+  }
+};
+
+const triggerFileInput = () => {
+  fileInput.value?.click();
+};
+</script>
