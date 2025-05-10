4902eaf114b0143d8e2a844357840a75bcdc6ae1
AlhoussinAmin <Alhoussin@34ml.com>
2025-05-10 21:27:45 +0300

    chore: move text properties out of text editor

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,6 +1,18 @@
+import type { ShallowRef } from "vue";
 export type TemplateRefType<T> = Readonly<ShallowRef<T | null>>;
 
 export type Size = {
   width: number;
   height: number;
+};
+
+export type TextConfigs = {
+  text: string;
+  fontFamily: string;
+  fontSize: number;
+  fontWeight: "normal" | "bold";
+  fontStyle: "" | "normal" | "italic" | "oblique";
+  underline: boolean;
+  fill: string;
+  stroke: string;
 };
