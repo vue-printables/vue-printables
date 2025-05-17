13314928bc52b7acc35282703e429720702675e9
ahmedd-mahmoud <ahmed.mahran831@gmail.com>
2025-05-17 19:06:21 +0300

    feat: update active image object

--- src/composables/useImage.ts
+++ src/composables/useImage.ts
@@ -1,19 +1,15 @@
-import { Control, FabricImage, type Canvas } from "fabric";
-import type { TemplateRefType } from "~/types/common";
+import { Control, FabricImage } from "fabric";
+import type { CanvasTemplateRef, ImgConfigs } from "~/types/common";
 import { renderDeleteControl } from "~/utils/fabricHelpers";
 
-export default function useImage(
-  canvasRef: TemplateRefType<{
-    canvasInstance: Canvas | null;
-  }>,
-) {
+export default function useImage(canvasRef: CanvasTemplateRef) {
   const addImage = (imgElement: HTMLImageElement) => {
     const image = new FabricImage(imgElement);
     image.controls.dd = new Control({
       x: 0.5,
       y: -0.5,
-      offsetY: -16,
-      offsetX: 16,
+      offsetY: -12,
+      offsetX: 12,
       cursorStyle: "pointer",
       render: renderDeleteControl,
       mouseDownHandler: () => {
@@ -24,5 +20,21 @@ export default function useImage(
     canvasRef.value?.canvasInstance?.centerObject(image);
   };
 
-  return { addImage };
+  const updateImage = (imgConfig: ImgConfigs) => {
+    const activeObj = canvasRef.value?.activeObj;
+    if (activeObj && activeObj?.type === "image") {
+      activeObj.set(imgConfig);
+      canvasRef.value?.canvasInstance?.renderAll();
+    } else {
+      if (activeObj && activeObj?.type !== "image") {
+        throw new Error(
+          `Active object select not of type "Image" is of type "${activeObj?.type}"`,
+        );
+      } else {
+        throw new Error("No active object selected");
+      }
+    }
+  };
+
+  return { addImage, updateImage };
 }
