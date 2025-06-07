a12495436a4b537cc90141adeda4561b11e62358
ahmedd-mahmoud <ahmed.mahran831@gmail.com>
2025-06-07 15:54:36 +0300

    feat: create useCanvas composable for creation/deletion of canvas

--- src/components/CanvasEditor.vue
+++ src/components/CanvasEditor.vue
@@ -18,11 +18,10 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, shallowRef, useTemplateRef } from "vue";
-import { Canvas, FabricObject, Rect } from "fabric";
+import { onMounted, onUnmounted, useTemplateRef } from "vue";
 import imageUrl from "~/assets/t-shirt.jpg";
 import type { Size } from "~/types/common";
-import { InteractiveFabricObject } from "fabric";
+import useCanvas from "~/composables/useCanvas";
 
 const props = withDefaults(
   defineProps<{
@@ -41,109 +40,25 @@ const props = withDefaults(
   },
 );
 
-const canvasInstance = shallowRef<Canvas | null>(null);
-const activeObj = shallowRef<FabricObject | null>(null);
-
 const mainCanvasRef = useTemplateRef("mainCanvas");
 
+const { fabricCanvas, activeObj, initCanvas, cleanup } =
+  useCanvas(mainCanvasRef);
+
 onMounted(async () => {
-  InteractiveFabricObject.ownDefaults = {
-    ...InteractiveFabricObject.ownDefaults,
-    hasControls: true,
-    cornerSize: 10,
-    cornerStrokeColor: "#0066cc",
-    cornerColor: "#99ccff",
-    cornerStyle: "circle",
-    transparentCorners: false,
-    cornerDashArray: null,
-    centeredScaling: true,
-    centeredRotation: true,
-    borderDashArray: [5, 2],
-  };
-
-  if (mainCanvasRef.value) {
-    // Create a "design area" rect that visually indicates the printable region
-    const printableArea = new Rect({
-      ...props.printableAreaSize,
-      strokeWidth: 3,
-      strokeUniform: true,
-      fill: "transparent",
-      stroke: "#ff6600",
-      borderScaleFactor: 2,
-      selectable: true,
-      setControlsVisibility: {
-        mtr: false, // Hide rotation control if not needed
-      },
-    });
-
-    const mainClipPath = await printableArea.clone();
-
-    mainClipPath.set({
-      width: printableArea.width + 30,
-      height: printableArea.height + 30,
-    });
-
-    canvasInstance.value = new Canvas(mainCanvasRef.value, {
-      ...props.size,
-      backgroundColor: "transparent",
-      selection: true,
-      preserveObjectStacking: true,
-      uniformScaling: false,
-      enableRetinaScaling: true,
-      clipPath: mainClipPath,
-    });
-
-    printableArea.on("scaling", async () => {
-      mainClipPath.set({
-        width: printableArea.width + 30,
-        height: printableArea.height + 30,
-        left: printableArea.left - 15,
-        top: printableArea.top - 15,
-        scaleX: printableArea.scaleX,
-        scaleY: printableArea.scaleY,
-      });
-    });
-
-    printableArea.on("moving", async () => {
-      mainClipPath.set({
-        width: printableArea.width + 30,
-        height: printableArea.height + 30,
-        left: printableArea.left - 15,
-        top: printableArea.top - 15,
-        scaleX: printableArea.scaleX,
-        scaleY: printableArea.scaleY,
-      });
-    });
-
-    canvasInstance.value.on("mouse:down", () => {
-      const obj = canvasInstance.value?.getActiveObject();
-
-      if (obj) {
-        activeObj.value = obj;
-      } else {
-        activeObj.value = null;
-      }
-    });
-
-    canvasInstance.value.add(printableArea);
-    canvasInstance.value.centerObject(printableArea);
-    canvasInstance.value.centerObject(mainClipPath);
-  } else {
-    throw new Error(
-      `Couldn't get canvas element with id:${mainCanvasRef.value}`,
-    );
-  }
+  await initCanvas({
+    productImageUrl: imageUrl,
+    canvasSize: props.size,
+    clipPathSize: props.printableAreaSize,
+  });
 });
 
 onUnmounted(async () => {
-  if (canvasInstance.value) {
-    await canvasInstance.value.dispose();
-    canvasInstance.value = null;
-  }
+  await cleanup();
 });
 
 defineExpose({
-  canvasInstance,
+  canvasInstance: fabricCanvas,
   activeObj,
 });
 </script>
